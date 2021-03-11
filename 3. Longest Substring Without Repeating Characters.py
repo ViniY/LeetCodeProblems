bd09eb2d480db941322e2aea1bf06b8474c09533
ViniY <38173292+ViniY@users.noreply.github.com>
2021-03-11 23:12:51 +1300

    Create 3. Longest Substring Without Repeating Characters.py
    not  finished

---
+++ 3. Longest Substring Without Repeating Characters.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        counter = 0
+        for i in range(len(s)):
+            pass
+
+
+
+if __name__ == '__main__':
+    s = Solution()
+    ss = "pwwkew"
+    r = s.lengthOfLongestSubstring(ss)
+    print(r)
