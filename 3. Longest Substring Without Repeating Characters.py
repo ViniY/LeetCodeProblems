c7b24019d6a7600efa578f3b3a2d00bfb6ce4f76
ViniY <38173292+ViniY@users.noreply.github.com>
2021-03-12 12:00:09 +1300

    Update 3. Longest Substring Without Repeating Characters.py

--- 3. Longest Substring Without Repeating Characters.py
+++ 3. Longest Substring Without Repeating Characters.py
@@ -4,14 +4,72 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        counter = 0
-        for i in range(len(s)):
-            pass
+        # counter = 0
+        # mx = 0
+        # start = 0
+        # m = set()
+        # for i in range(len(s)):
+        #     if s[i] not in m:
+        #         m.add(s[i])     # Add to a set
+        #         counter += 1      # increase the counter by one
+        #         print(i)
+        #     else:
+        #         m.clear()
+        #         if mx < counter:
+        #             mx = counter
+        #         counter = 0
+        #         start += 1
+        #         i = int(start)
+        #         i=i-1
+        #         print(i)
+
+        # i=0
+
+        # while i< len(s):
+        #     if s[i] not in m:
+        #         m.add(s[i])     # Add to a set
+        #         counter += 1      # increase the counter by one
+        #         i += 1
+        #     else:
+        #         m.clear()
+        #         if mx < counter:
+        #             mx = counter
+        #         counter = 0
+        #         start += 1
+        #         i = int(start)
+
+
+        left = 0
+        right = 0
+        used = set()
+        mx = 0
+        if len(set(s)) == len(s): return len(s)
+        if len(set(s)) == 1: return 1
+        for i, c in enumerate(s):
+            if c not in used:
+                used.add(c)
+                right += 1
+            else:
+                used.clear()
+                mx = right - left
+                left = i
+                right = left
+
+        if mx > (right - left):
+            return mx
+        else:
+            return right - left
+
+        # if counter>mx: mx=counter
+        # print(m)
+        # return mx
+
 
 
 
 if __name__ == '__main__':
     s = Solution()
     ss = "pwwkew"
+    # ss = "aab"
     r = s.lengthOfLongestSubstring(ss)
     print(r)
